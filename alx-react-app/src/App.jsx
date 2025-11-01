92e90a9c46e24e3ce4e308e90c26cbaec2fbfddc
sami36996 <davidtake05@gmail.com>
2025-11-01 07:53:59 +0300

    Add Vite React app setup

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      {/* 👇 Add this line to show your custom component */}
+      <WelcomeMessage />
+
+    </>
+  )
+}
+
+export default App
+
