29683a12ea9eb987eac81235fb4ee213353fab1d
sami36996 <davidtake05@gmail.com>
2025-11-22 16:27:54 +0300

    Add favorites & recommendations to store

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -2,9 +2,22 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  favourites: [],
-  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
-  addFavourite: (recipe) => set((state) => ({ favourites: [...state.favourites, recipe] })),
-  setFavourites: (favourites) => set({ favourites }),
+  favorites: [],           // <--- add this
+  recommendations: [],     // <--- add this
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  setRecipes: (recipes) => set({ recipes }),
+
+  addFavorite: (recipe) =>
+    set((state) => ({ favorites: [...state.favorites, recipe] })),
+
+  removeFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: state.favorites.filter((r) => r.id !== recipeId),
+    })),
+
+  setRecommendations: (recommendations) => set({ recommendations }),
 }));
 
